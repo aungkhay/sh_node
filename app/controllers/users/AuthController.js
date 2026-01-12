776728fa21218d950f69a24b916f89d5f6272a68
aungkhay <saiaungkhaylamm@gmail.com>
2026-01-12 19:13:12 +0800

    Fix

--- app/controllers/users/AuthController.js
+++ app/controllers/users/AuthController.js
@@ -24,7 +24,18 @@ class Controller {
     }
 
     GET_RECAPTCHA = async (req, res) => {
+        const lockKey = `lock:get_recaptcha:${req.user_id}`;
+        let redisLocked = false;
+        
         try {
+            /* ===============================
+            * REDIS LOCK (ANTI FAST-CLICK)
+            * =============================== */
+            redisLocked = await this.redisHelper.setLock(lockKey, 1, 1);
+            if (redisLocked !== 'OK') {
+                return MyResponse(res, this.ResCode.BAD_REQUEST.code, false, '操作过快，请稍后再试', {});
+            }
+
             const key = uuidv4();
 
              const captcha = svgCaptcha.create({
