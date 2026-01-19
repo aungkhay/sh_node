223b18d963a1083eafad8fe5bc8b8d07aaee9bd6
aungkhay <saiaungkhaylamm@gmail.com>
2026-01-20 00:17:09 +0800

    Fix

--- app/controllers/users/HomeController.js
+++ app/controllers/users/HomeController.js
@@ -1632,8 +1632,8 @@ class Controller {
             }
             await this.redisHelper.setValue(`UID_${userId}_reward`, JSON.stringify(data), 5 * 60); // 5 minutes
 
-            // const QUEUE_KEY = 'QUEUE:RED_ENVELOP_POST_PROCESS';
-            // await this.redisHelper.rPushValue(QUEUE_KEY, JSON.stringify(data));
+            const QUEUE_KEY = 'QUEUE:RED_ENVELOP_POST_PROCESS';
+            await this.redisHelper.rPushValue(QUEUE_KEY, JSON.stringify(data));
 
             return MyResponse(res, this.ResCode.SUCCESS.code, true, '成功', data);
         } catch (error) {
@@ -1643,11 +1643,12 @@ class Controller {
     }
 
     GET_RED_ENVELOP = async (req, res) => {
+        return MyResponse(res, this.ResCode.SUCCESS.code, true, '领取成功！5分钟后可以在道具里面查看', {});
+        
         const userId = req.user_id;
         const lockKey = `lock:get-red-envelope:${userId}`;
 
         try {
-            // return MyResponse(res, this.ResCode.SUCCESS.code, true, '领取成功！5分钟后可以在道具里面查看', {});
 
             /* ===============================
             * REDIS LOCK (ANTI FAST-CLICK)
