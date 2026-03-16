dfc88b62bf37759886cb5b603465ebf2298079e8
aungkhay <saiaungkhaylamm@gmail.com>
2026-03-16 13:23:57 +0800

    Fix

--- app/controllers/admins/UserController.js
+++ app/controllers/admins/UserController.js
@@ -2446,22 +2446,22 @@ class Controller {
             });
 
             // Sign Agreement [签署协议]
-            const signAgreement = [];
-            if (user.agreement_status === 'APPROVED') {
-                const record7 = await RewardRecord.findOne({
-                    where: { user_id: user.id, reward_id: 7, amount: 20, from_where: '签署电子协议 获得上合战略储备黄金券' },
-                    attributes: ['id', 'createdAt']
-                });
-                    if (record7) {
-                        signAgreement.push({
-                        id: record7.id,
-                        amount: 100,
-                        createdAt: record7.createdAt,
-                        type: '签署协议',
-                        description: '扣除 100 储备金'
-                    });
-                }
-            }
+            // const signAgreement = [];
+            // if (user.agreement_status === 'APPROVED') {
+            //     const record7 = await RewardRecord.findOne({
+            //         where: { user_id: user.id, reward_id: 7, amount: 20, from_where: '签署电子协议 获得上合战略储备黄金券' },
+            //         attributes: ['id', 'createdAt']
+            //     });
+            //         if (record7) {
+            //             signAgreement.push({
+            //             id: record7.id,
+            //             amount: 100,
+            //             createdAt: record7.createdAt,
+            //             type: '签署协议',
+            //             description: '扣除 100 储备金'
+            //         });
+            //     }
+            // }
             
             // Withdrawals [提现]
             const withdrawals = await Withdraw.findAll({
