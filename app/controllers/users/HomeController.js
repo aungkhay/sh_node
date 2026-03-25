14b4baf3a55dcbd803ccd648703116f771501d10
aungkhay <saiaungkhaylamm@gmail.com>
2026-03-25 23:25:16 +0800

    Fix

--- app/controllers/users/HomeController.js
+++ app/controllers/users/HomeController.js
@@ -3531,7 +3531,7 @@ class Controller {
 
             const currentDate = new Date();
             const repurchaseStartDate = new Date('2024-04-09 00:00:00');
-            if (currentDate < repurchaseStartDate) {
+            if (currentDate.getTime() < repurchaseStartDate.getTime()) {
                 return MyResponse(res, this.ResCode.BAD_REQUEST.code, false, '首批黄金券回购预计在4月9日开放，15个工作日内完成回购拨款，持有和衷联储黄金礼包（第二批）的用户可获得首批回购资格，回购价格预计为980元/克', {});
             }
 
@@ -3585,7 +3585,7 @@ class Controller {
         try {
             const currentDate = new Date();
             const repurchaseStartDate = new Date('2024-04-09 00:00:00');
-            if (currentDate < repurchaseStartDate) {
+            if (currentDate.getTime() < repurchaseStartDate.getTime()) {
                 return MyResponse(res, this.ResCode.BAD_REQUEST.code, false, '首批黄金券回购预计在4月9日开放，15个工作日内完成回购拨款，持有和衷联储黄金礼包（第二批）的用户可获得首批回购资格，回购价格预计为980元/克', {});
             }
 
