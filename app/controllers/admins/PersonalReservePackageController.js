434e025362a3bf33cdef060aafe2f6f865a6a08a
aungkhay <saiaungkhaylamm@gmail.com>
2026-07-08 19:20:24 +0800

    Fix

--- app/controllers/admins/PersonalReservePackageController.js
+++ app/controllers/admins/PersonalReservePackageController.js
@@ -83,7 +83,7 @@ class Controller {
                 return MyResponse(res, this.ResCode.VALIDATE_FAIL.code, false, this.ResCode.VALIDATE_FAIL.msg, {}, errors);
             }
 
-            const { product_name, price, period, reserve_earn, release_earn_count, release_earn_count, release_personal_gold_rate, is_release_authorize_letter, purchase_limit, quantity_limit, total_quantity, description, buy_one_get_quantity, can_new_registered_user_get_free } = req.body;
+            const { product_name, price, period, reserve_earn, release_earn_count, release_personal_gold_rate, is_release_authorize_letter, purchase_limit, quantity_limit, total_quantity, description, buy_one_get_quantity, can_new_registered_user_get_free } = req.body;
             const newPackage = await PersonalReservePackage.create({
                 product_name: product_name,
                 price: price,
@@ -128,7 +128,7 @@ class Controller {
                 return MyResponse(res, this.ResCode.NOT_FOUND.code, false, '未找到信息', {});
             }
 
-            const { product_name, price, period, reserve_earn, release_earn_count, release_earn_count, release_personal_gold_rate, is_release_authorize_letter, purchase_limit, quantity_limit, total_quantity, description, buy_one_get_quantity, can_new_registered_user_get_free } = req.body;
+            const { product_name, price, period, reserve_earn, release_earn_count, release_personal_gold_rate, is_release_authorize_letter, purchase_limit, quantity_limit, total_quantity, description, buy_one_get_quantity, can_new_registered_user_get_free } = req.body;
             await pkg.update({
                 product_name: product_name,
                 price: price,
