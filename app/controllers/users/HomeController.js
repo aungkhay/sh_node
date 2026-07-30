24cf57e1a8887a1245ae670dbaeed50b485c2d92
aungkhay <saiaungkhaylamm@gmail.com>
2026-07-30 19:58:30 +0800

    Fix

--- app/controllers/users/HomeController.js
+++ app/controllers/users/HomeController.js
@@ -11396,7 +11396,7 @@ class Controller {
                     },
                     useMaster: true
                 });
-                if (historyCount >= aPackage.quantity_limit) {
+                if (historyCount > aPackage.quantity_limit) {
                     await this.redisHelper.deleteKey(PROCESSING_KEY);
                     return MyResponse(res, this.ResCode.BAD_REQUEST.code, false, '今日已购买过该方案', {});
                 }
