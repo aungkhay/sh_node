dcc60b90434881950698be8e360aea93ce0dd9cc
aungkhay <saiaungkhaylamm@gmail.com>
2026-08-18 05:23:46 +0800

    Fix: Cron

--- app/cron/index.js
+++ app/cron/index.js
@@ -61,7 +61,7 @@ class CronJob {
         cron.schedule('30 23 * * *', this.RESET_REWARD_TYPE).start();
         // cron.schedule('30 23 * * *', this.CHECK_GOLD_PACKAGE_DAILY_RETURN).start();
         cron.schedule('10 0 * * *', this.GIVE_MASONIC_BONUS).start();
-        cron.schedule('25 0 * * *', this.GIVE_POLICY_PACKAGE_EARN).start(); // all completed
+        // cron.schedule('25 0 * * *', this.GIVE_POLICY_PACKAGE_EARN).start(); // all completed
         // Every 10 minutes
         cron.schedule('*/10 * * * *', this.SUBSTRACT_MASONIC_FUND).start();
         // Run 10th minute of every hour
@@ -93,14 +93,14 @@ class CronJob {
         // Run at 1AM Every day
         // cron.schedule('30 0 15 * *', this.CHECK_GOLD_APPRECIATION_PACKAGE_RETURN_EARN).start(); // manual
         // cron.schedule('0 1 * * *', this.CHECK_GOLD_APPRECIATION_PACKAGE_REIMBURSEMENT).start(); // all completed
-        cron.schedule('40 0 * * *', this.CALCULATE_ASSET_EARN).start();
-        cron.schedule('30 1 * * *', this.RELEASE_ASSET_FUND).start();
-        cron.schedule('0 2 * * *', this.RELEASE_ASSET_EARN).start();
-        cron.schedule('30 2 * * *', this.ASSET_DAILY_RELEASE_EARN).start();
-        cron.schedule('0 3 * * *', this.ASSET_DAILY_RELEASE_ORIGINAL_PRICE).start();
-        cron.schedule('30 3 * * *', this.RELEASE_EXTRA_DISTRIBUTION).start();
-        cron.schedule('0 4 * * *', this.ASSET_DAILY_RELEASE_EXTRA_PACKAGE).start();
-        cron.schedule('30 4 * * *', this.RELEASE_SCO_VERIFIED_ASSETS).start();
+        cron.schedule('30 0 * * *', this.CALCULATE_ASSET_EARN).start();
+        // cron.schedule('30 1 * * *', this.RELEASE_ASSET_FUND).start();
+        // cron.schedule('0 2 * * *', this.RELEASE_ASSET_EARN).start();
+        // cron.schedule('30 2 * * *', this.ASSET_DAILY_RELEASE_EARN).start();
+        // cron.schedule('0 3 * * *', this.ASSET_DAILY_RELEASE_ORIGINAL_PRICE).start();
+        // cron.schedule('30 3 * * *', this.RELEASE_EXTRA_DISTRIBUTION).start();
+        // cron.schedule('0 4 * * *', this.ASSET_DAILY_RELEASE_EXTRA_PACKAGE).start();
+        cron.schedule('20 1 * * *', this.RELEASE_SCO_VERIFIED_ASSETS).start();
     }
 
     PAY_ALLOWANCE = async () => {
